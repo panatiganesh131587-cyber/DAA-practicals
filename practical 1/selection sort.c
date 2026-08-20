d7ad79332a2afdc57da6e4eacfce800fb486e48d
panatiganesh131587-cyber <panatiganesh.131587@marwadiuniversity.ac.in>
2026-08-19 21:40:56 -0700

    Update selection sort.c

--- practical 1/selection sort.c
+++ practical 1/selection sort.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
-int main() {
-    int arr[] = {50, 10, 40, 20, 30};
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d numbers:\n", n);
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
         int min = i;
-        for (int j = i + 1; j < 5; j++) {
-            if (arr[j] < arr[min]) {
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
                 min = j;
             }
         }
+
         int temp = arr[i];
         arr[i] = arr[min];
         arr[min] = temp;
     }
 
-    printf("ascending order: ");
-    for (int i = 0; i < 5; i++) {
+    printf("Ascending order: ");
+
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
 
