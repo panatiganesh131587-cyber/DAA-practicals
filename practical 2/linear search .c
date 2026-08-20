f47ab06bf7ccb8a47302cef81531b708c5a04d0d
panatiganesh131587-cyber <panatiganesh.131587@marwadiuniversity.ac.in>
2026-08-19 21:47:34 -0700

    Refactor linear search to accept dynamic input

--- practical 2/linear search .c
+++ practical 2/linear search .c
@@ -1,15 +1,25 @@
 #include <stdio.h>
-int main() {
-    int arr[] = {10, 20, 30, 40, 50};
-    int n = 30; 
-
-    for (int i = 0; i < 5; i++) {
-        if (arr[i] == n) {
+int main()
+{
+    int n, key;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d numbers:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
             printf("Found at index %d\n", i);
-            return 0; // Stop the program once found
+            return 0;
         }
     }
-
     printf("Not found\n");
     return 0;
 }
