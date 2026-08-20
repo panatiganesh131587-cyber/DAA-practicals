69dc2de206d5cdbe233476f351fbbae3afb81b98
panatiganesh131587-cyber <panatiganesh.131587@marwadiuniversity.ac.in>
2026-08-19 21:22:40 -0700

    Update binary search program for user input
    Modified the program to allow user input for array elements and the search key.

--- practical 1/bubble sort.c
+++ practical 1/bubble sort.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
-int main() {
-    int a[5] = {10, 20, 30, 40, 50};
-    int low = 0, high = 4, mid, key = 30;
+int main()
+{
+    int a[5];
+    int low = 0, high = 4, mid, key;
 
-    while (low <= high) {
+    printf("Enter 5 elements in sorted order:\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    while (low <= high)
+    {
         mid = (low + high) / 2;
 
-        if (a[mid] == key) {
+        if (a[mid] == key)
+        {
             printf("Element found");
             return 0;
         }
         else if (key > a[mid])
+        {
             low = mid + 1;
+        }
         else
+        {
             high = mid - 1;
+        }
     }
 
     printf("Element not found");
+
     return 0;
 }
