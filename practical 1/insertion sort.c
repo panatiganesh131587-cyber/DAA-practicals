50c9c6bb673a5d1bd046399606d8fdb93dd41089
panatiganesh131587-cyber <panatiganesh.131587@marwadiuniversity.ac.in>
2026-08-19 21:35:47 -0700

    Update insertion sort to accept user input
    Modified the insertion sort program to allow user input for the array elements.

--- practical 1/insertion sort.c
+++ practical 1/insertion sort.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
-int main() {
-    int arr[] = {50, 10, 40, 20, 30};
-    for (int i = 1; i < 5; i++) {
+int main()
+{
+    int arr[5];
+
+    printf("Enter 5 elements:\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 1; i < 5; i++)
+    {
         int key = arr[i];
         int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
+
+        while (j >= 0 && arr[j] > key)
+        {
             arr[j + 1] = arr[j];
             j--;
         }
+
         arr[j + 1] = key;
     }
-    printf("ascending order: ");
-    for (int i = 0; i < 5; i++) {
+
+    printf("Ascending order: ");
+
+    for (int i = 0; i < 5; i++)
+    {
         printf("%d ", arr[i]);
     }
+
     return 0;
 }
